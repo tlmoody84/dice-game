faa105534c9d4ef798ebcd2a0f66c916ec2202a7
tlmoody84 <156528348+tlmoody84@users.noreply.github.com>
2024-07-22 22:58:34 -0400

    change nav bar color

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header className="w-100 bg-emerald-800">
+        <header className="w-100 bg-blue-400">
           <nav className="flex justify-center w-100 ">
             <Link
               className="m-1 text-purple-700 hover:text-pink-700"
