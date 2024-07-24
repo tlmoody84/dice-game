434963c67f90e2c81aad9e518fc458224f449013
tlmoody84 <156528348+tlmoody84@users.noreply.github.com>
2024-07-23 22:29:20 -0400

    created an export for auth on firebaseconfig

--- firebase.config.js
+++ firebase.config.js
@@ -24,5 +24,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 const db = getFirestore(app)
-const auth = getAuth(app);
+export const auth = getAuth(app);
 export { db }
+
